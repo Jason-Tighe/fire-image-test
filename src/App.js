686f38145d428d08dd0b1b5f0e8bbf614e0dd54f
slowdolphin <tighe59@gmail.com>
2021-12-12 20:20:06 -0500

    working through craeting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import {db} from './firebase-config'
-import {collection, getDocs} from 'firebase/firestore'
+import {collection, getDocs, addDoc} from 'firebase/firestore'
 import {useState, useEffect} from 'react'
 
 function App() {
   const [fileTest, setFileTest] = useState([])
-  //"test" is the state
+  //"test" is the  database and state. Let's keep this going like so.
   const fileTestCollectionRef = collection(db, "fileTest")
+  const [newTitle, setNewTitle] = useState("")
+  const [newDocInfo, setNewDocInfo] = useState("")
+
+  const handleTitleChange = e => {
+  setNewTitle({ ...newTitle, [e.target.name]: e.target.value });
+};
+
+const handleDocInfoChange = e => {
+setNewDocInfo({ ...newDocInfo, [e.target.name]: e.target.value});
+};
 
   useEffect(()=>{
+    //this is getting and setting all items from the collection. I'll refernce how i've written stuff like this before.
     const getFileTest = async () =>{
       const data = await getDocs(fileTestCollectionRef)
       console.log(data)
@@ -16,10 +27,27 @@ function App() {
 
     getFileTest()
   },[])
-
-
+//have to look at this
+  const createTest = async () => {
+    await addDoc(fileTestCollectionRef, {Title: newTitle, docInfo: newDocInfo})
+  }
   return (
     <div className="App">
+    <input
+      name="title"
+      placeholder="Title"
+      onChange={handleTitleChange}
+    />
+    <input
+      name="docInfo"
+      placeholder="DocInfo"
+      onChange={handleDocInfoChange}
+    />
+    <button onClick={createTest}> Create User</button>
+
+    {//this is itterating through the array of items in the collection. There is only 1 atm.
+    //things to build out is the rest of the CRUD.
+    }
       {fileTest.map((item)=>{
         return(
           <div key={item.id}>
