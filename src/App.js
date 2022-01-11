efc6cbdc0d692a5a9b979d09e86267c7efc0548b
slowdolphin <tighe59@gmail.com>
2022-01-10 22:59:00 -0500

    some notes made

--- src/App.js
+++ src/App.js
@@ -38,28 +38,37 @@ function App() {
   const [image, setImage] = useState(null);
   //7
   const [files, setFiles] = useState();
-//8
+  //8
   const [cubeFace, setCubeFace] = useState({
     link: "",
   });
-//This uploads the image and then generates the url and sets it.
-  const handleLinkUpload = () => {
+  //This uploads the image and then generates the url and sets it.
+
+  //need to check URItoBlob
+  const handleLinkUpload = async () => {
     const qrStorageRef = ref(storage, "QrImage/" + newTitle);
-    const uploadTask = uploadString(qrStorageRef, qrCode, "data_url")
-    getDownloadURL(qrStorageRef).then((url) => {
-      setLink(url)
-      console.log(url)
-      // createTest()
-    });
+    const uploadTask = uploadString(qrStorageRef, qrCode, "data_url");
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        console.log("oops");
+      },
+      () => {
+        getDownloadURL(qrStorageRef.snapshot.ref).then((url) => {
+          setLink(url);
+          console.log(url);
+          // createTest()
+        });
+      }
+    );
   };
 
-
   const createQrImage = () => {
-
     let canvas = qrRef.current.querySelector("canvas");
     let qrFile = canvas.toDataURL("image/png");
     setQrCode(qrFile);
-    // handleLinkUpload(qrFile);
+    handleLinkUpload(qrFile);
     //clear states
     setCubeFace({
       link: "",
@@ -108,7 +117,6 @@ function App() {
     setNewDocInfo({ ...newDocInfo, [e.target.name]: e.target.value });
   };
 
-
   // creating the storage, .ref is the refernce creating a new folder id, ".put" uploading.
   // .on, snapshot = current progress, error checking,
   const handleUpload = async () => {
@@ -138,55 +146,55 @@ function App() {
     );
   };
 
-
-
   const updateTest = async (id, docInfo) => {
     const testDoc = doc(db, "fileTest", id);
     const newFields = { docInfo: "" };
     await updateDoc(testDoc, newFields);
   };
 
-
   const deleteTest = async (id) => {
     const testDoc = doc(db, "fileTest", id);
     await deleteDoc(testDoc);
-    deleteStorageTest()
-    deleteImgStorageTest()
+    deleteStorageTest();
+    deleteImgStorageTest();
   };
 
   // Create a reference to the file to delete
-const deleteStorageTest  = async () => {
-const deleteQrStorageRef = ref(storage, "QrImage/" + newTitle);
+  const deleteStorageTest = async () => {
+    const deleteQrStorageRef = ref(storage, "QrImage/" + newTitle);
 
-// Delete the file
-deleteObject(deleteQrStorageRef).then(() => {
-  console.log("nice")
-}).catch((error) => {
-  console.log("you suck")
-})
-}
-const deleteImgStorageTest  = async () => {
-const deleteImgStorageRef = ref(storage, "Images/" + newTitle);
+    // Delete the file
+    deleteObject(deleteQrStorageRef)
+      .then(() => {
+        console.log("nice");
+      })
+      .catch((error) => {
+        console.log("you suck");
+      });
+  };
+  const deleteImgStorageTest = async () => {
+    const deleteImgStorageRef = ref(storage, "Images/" + newTitle);
 
-// Delete the file
-deleteObject(deleteImgStorageRef).then(() => {
-  console.log("nice")
-}).catch((error) => {
-  console.log("you suck")
-})
-}
+    // Delete the file
+    deleteObject(deleteImgStorageRef)
+      .then(() => {
+        console.log("nice");
+      })
+      .catch((error) => {
+        console.log("you suck");
+      });
+  };
 
   const getFileTest = async () => {
     const abc = onSnapshot(fileTestCollectionRef, (querySnapshot) => {
       const items = [];
       querySnapshot.forEach((doc) => {
         // snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))
-        items.push({id: doc.id, ...doc.data()});
+        items.push({ id: doc.id, ...doc.data() });
       });
       setFileTest(items);
     });
   };
-
 
   useEffect(() => {
     getFileTest();
@@ -201,6 +209,17 @@ deleteObject(deleteImgStorageRef).then(() => {
     });
   };
 
+  useEffect(() => {
+    const createTest = async () => {
+      await addDoc(fileTestCollectionRef, {
+        Title: newTitle,
+        DocInfo: newDocInfo,
+        Link: link,
+      });
+    };
+    createTest();
+  }, [link]);
+
   const [dataB, setDataB] = useState(false);
   const closeDataList = () => {
     setDataB(!dataB);
@@ -211,7 +230,6 @@ deleteObject(deleteImgStorageRef).then(() => {
   };
 
   const code = <QRCode level="Q" value={cubeFace.link} />;
-
 
   return (
     <div className="App">
@@ -242,7 +260,6 @@ deleteObject(deleteImgStorageRef).then(() => {
           <button onClick={createQrImage}> Create Image</button>
           <button onClick={handleLinkUpload}> Upload Image to Storage</button>
           <button onClick={createTest}> Upload to Database</button>
-
 
           {fileTest.map((item, index) => {
             return (
